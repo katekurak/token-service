58ee4fd3dce352f1e8fe9b0113609fa497dc328c
kate03k <kateryna.kurak@upc.ua>
2025-04-10 16:41:53 +0300

    2.4.v

---
+++ sources/iW5QSjmH.js
@@ -0,0 +1,16 @@
+import{d as a,H as i,o as c,ae as s,t as r,v as l,x as n,B as o}from"./DhQvyrGS.js";const m={class:"code-highlighting"},h=a({__name:"CodeHighlighting",setup(u){const{t:e}=i();return c(()=>{s.plugins.toolbar.registerButton("try-it-out-button",{text:e("buttons.tryItOut"),onClick:()=>{window.open(e("links.openApi"),"_blank")}}),s.highlightAll()}),(g,t)=>(l(),r("div",m,t[0]||(t[0]=[n("pre",{class:"line-numbers"},[o("      "),n("code",{class:"language-html"},`<?xml version="1.0" encoding="UTF-8"?>
+      <ECommerceConnect
+        xmlns:xenc="http://www.w3.org/2001/04/xmlenc#"
+        xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
+        xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+        xsi:noNamespaceSchemaLocation="https://secure.upc.ua/go/pub/schema/xmlpay-1.24.xsd">
+        <Message id="123" version='1'>
+          <XMLPayRequest>
+            <RequestData>
+              <MerchantID>1750000</MerchantID>
+              <TerminalID>E1750000</TerminalID>
+            </RequestData>
+          </XMLPayRequest>
+        </Message>
+      </ECommerceConnect>`),o(`
+    `)],-1)])))}});export{h as default};
