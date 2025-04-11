5c0bae135c67a70f4ce563809b492a6d6a1f155e
kate03k <kateryna.kurak@upc.ua>
2025-04-11 11:46:07 +0300

    2.3.v w3c html

---
+++ sources/_7q_4vBK.js
@@ -0,0 +1,1 @@
+import{a9 as n,aa as s,R as r,D as o,ab as i}from"./BPKU-ukn.js";function u(a){var t;const e=a||s();return((t=e==null?void 0:e.ssrContext)==null?void 0:t.head)||(e==null?void 0:e.runWithContext(()=>{if(r())return o(i)}))}function d(a,e={}){const t=u(e.nuxt);if(t)return n(a,{head:t,...e})}export{d as u};
--- sources/lA9URVVT.js
+++ sources/lA9URVVT.js
@@ -1,4 +1,4 @@
-import{d as a,H as i,o as c,af as s,t as r,v as l,x as n,B as o}from"./DQM6EhcG.js";const m={class:"code-highlighting"},h=a({__name:"CodeHighlighting",setup(u){const{t:e}=i();return c(()=>{s.plugins.toolbar.registerButton("try-it-out-button",{text:e("buttons.tryItOut"),onClick:()=>{window.open(e("links.openApi"),"_blank")}}),s.highlightAll()}),(g,t)=>(l(),r("div",m,t[0]||(t[0]=[n("pre",{class:"line-numbers"},[o("      "),n("code",{class:"language-html"},`<?xml version="1.0" encoding="UTF-8"?>
+import{d as a,H as i,o as c,af as s,t as r,v as l,x as n,B as o}from"./BPKU-ukn.js";const m={class:"code-highlighting"},h=a({__name:"CodeHighlighting",setup(u){const{t:e}=i();return c(()=>{s.plugins.toolbar.registerButton("try-it-out-button",{text:e("buttons.tryItOut"),onClick:()=>{window.open(e("links.openApi"),"_blank")}}),s.highlightAll()}),(g,t)=>(l(),r("div",m,t[0]||(t[0]=[n("pre",{class:"line-numbers"},[o("      "),n("code",{class:"language-html"},`<?xml version="1.0" encoding="UTF-8"?>
       <ECommerceConnect
         xmlns:xenc="http://www.w3.org/2001/04/xmlenc#"
         xmlns:ds="http://www.w3.org/2000/09/xmldsig#"
